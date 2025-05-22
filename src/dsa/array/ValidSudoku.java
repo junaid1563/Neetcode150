6efd51a6099ea3bb543a9cb7e424429feca02754
junaid1563 <junaidajmer87@gmail.com>
2025-05-23 00:15:10 +0530

    Refactor grid validation logic in ValidSudoku to improve readability and correctness

--- src/dsa/array/ValidSudoku.java
+++ src/dsa/array/ValidSudoku.java
@@ -25,21 +25,22 @@ public class ValidSudoku {
 				}
 			}
 		}
+		// validate grid
 		for (int i = 0; i < board.length; i += 3) {
 			List<Character> gridVal = new ArrayList<>();
-			for (int row = i; row <= i + 2; row++) {
-				for (int j = 0; j < board[row].length; j += 3) {
+			for (int j = 0; j < board[i].length; j += 3) {
+				for (int row = i; row <= i + 2; row++) {
 					for (int col = j; col <= j + 2; col++) {
-						if (gridVal.contains(board[col][row])) {
+						if (gridVal.contains(board[row][col])) {
 							return false;
 						}
-						if (board[col][row] != '.') {
-							gridVal.add(board[col][row]);
+						if (board[row][col] != '.') {
+							gridVal.add(board[row][col]);
 						}
 					}
 
 				}
-				System.out.println("grid = " + gridVal.toString());
+				// System.out.println("grid = " + gridVal.toString());
 				gridVal = new ArrayList<>();
 
 			}
